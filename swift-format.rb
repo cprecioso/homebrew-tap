c6a1b18cc2fe02641e56b57c4a61abdf7511f971
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2020-11-14 16:06:00 +0100

    Remove bottle

--- swift-format.rb
+++ swift-format.rb
@@ -8,11 +8,6 @@ class SwiftFormat < Formula
   version_scheme 1
   head "https://github.com/apple/swift-format.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d8f72c33efc125e2904e1bec2c8942cca75d75cf81dcab7fcf08ba124af16170" => :catalina
-  end
-
   depends_on xcode: ["12"]
 
   def install
